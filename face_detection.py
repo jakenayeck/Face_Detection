e6b8a10f2b431e8b61ba9b6cfd83f1e06155f189
jakenayeck <jan2150@columbia.edu>
2015-08-02 18:21:24 -0400

    wasn't working with webcam soI tried an image of obama, found a bug with the path file

--- face_detection.py
+++ face_detection.py
@@ -1,17 +1,19 @@
 import numpy as np
 import cv2
 
-PATH_FACE = "haarcascade/haarcascade_frontalface_default.xml"
+PATH_FACE = 'haarcascade/haarcascade_frontalface_default.xml'
+#PATH_FACE = "haarcascade/haarcascade_frontalface_alt.xml"
 face_cascade = cv2.CascadeClassifier(PATH_FACE)
 print face_cascade.empty()
-webcam = cv2.VideoCapture(0)
-webcam.set(3, 640)
-webcam.set(4, 480)
+#webcam = cv2.VideoCapture(0)
+#webcam.set(3, 640)
+#webcam.set(4, 480)
 while True:
-    ret, frame = webcam.read()
+    #ret, frame = webcam.read()
+    frame      = cv2.imread('obama.png')
     gray       = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=5, minSize= (30,30) , flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=2, minSize= (30,30) , flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
     print faces
     
     cv2.imshow('gray', gray)
