4ca5107ff809f2f9da8914d519d5f4ee4c731ea5
jakenayeck <jan2150@columbia.edu>
2015-08-04 21:14:58 -0400

    restructured code

--- face_detection.py
+++ face_detection.py
@@ -1,20 +1,43 @@
 import numpy as np
 import cv2
+import sys
 
-PATH_FACE = 'haarcascade/haarcascade_frontalface_default.xml'
-#PATH_FACE = "haarcascade/haarcascade_frontalface_alt.xml"
-face_cascade = cv2.CascadeClassifier(PATH_FACE)
-webcam = cv2.VideoCapture(0)
-webcam.set(3, 640)
-webcam.set(4, 480)
-while True:
+def initialize_camera():
+    webcam = cv2.VideoCapture(0)
+    webcam.set(3, 640)
+    webcam.set(4, 480)
+    return webcam
+
+def get_frames(webcam):
     ret, frame = webcam.read()
     gray       = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=2, minSize= (30,30) , flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+    return frame, gray
+
+def draw_rectangle_over_faces(img, faces):
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
-    cv2.imshow('frame', frame)
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+    cv2.imshow('frame', color_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-webcam.release()
-cv2.destroyAllWindows()
+        webcam.release()
+        cv2.destroyAllWindows()
+        sys.exit()
+def create_face_learner():
+    PATH_FACE = 'haarcascade/haarcascade_frontalface_default.xml'
+    face_learner = cv2.CascadeClassifier(PATH_FACE)
+    return face_learner
+
+def find_faces(gray_image):
+    faces = face_learner.detectMultiScale(gray_image, scaleFactor = 1.1, minNeighbors=2, minSize= (30,30) )
+    return faces
+
+################
+## Main Loop  ##
+################
+
+webcam = initialize_camera()
+face_learner = create_face_learner()
+while True:
+    color_image, gray_image = get_frames(webcam)
+    faces = find_faces(gray_image)
+    draw_rectangle_over_faces(color_image, faces)
+
